6bf1c784090d43d9c990f4778b6ec75cc862341f
S.Watanabe <ocelot2828@gmail.com>
2023-06-07 14:18:00 +0900

    perf(*): remove styles

--- src/components/PageRoot/index.tsx
+++ src/components/PageRoot/index.tsx
@@ -1,25 +1,12 @@
-'use client'
-
-import type { ReactNode } from 'react'
+import { ReactNode } from 'react'
 import styles from './styles.module.scss'
-import useFitter, { Main } from 'react-page-fitter'
-import { usePathname } from 'next/navigation'
 
 type PageRootProps = {
   children: ReactNode
 }
 
 const PageRoot = ({ children }: PageRootProps) => {
-  const pathname = usePathname()
-  const isFit = useFitter()
-
-  if (isFit === undefined) return <div className={styles.spinner} />
-
-  return (
-    <Main location={pathname} className={styles.pages_root} classFitIn={styles.fit_in} classFitOut={styles.fit_out}>
-      {children}
-    </Main>
-  )
+  return <main className={styles.pages_root}>{children}</main>
 }
 
 export default PageRoot
