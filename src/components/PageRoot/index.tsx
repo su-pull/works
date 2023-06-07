8193e9bd2fd3426c512dc5a5c67b2205534c1cae
S.Watanabe <ocelot2828@gmail.com>
2023-06-07 21:22:35 +0900

    feat(ScrollToTop): add

--- src/components/PageRoot/index.tsx
+++ src/components/PageRoot/index.tsx
@@ -1,12 +1,23 @@
-import { ReactNode } from 'react'
+'use client'
+
+import { ReactNode, useRef } from 'react'
 import styles from './styles.module.scss'
+import ScrollToTop from 'components/ScrollToTop'
+import useFitter from 'react-page-fitter'
 
 type PageRootProps = {
   children: ReactNode
 }
 
 const PageRoot = ({ children }: PageRootProps) => {
-  return <main className={styles.pages_root}>{children}</main>
+  const ref = useRef(null)
+  const isFit = useFitter(ref)
+  return (
+    <main className={styles.pages_root} ref={ref}>
+      {children}
+      {!isFit && <ScrollToTop />}
+    </main>
+  )
 }
 
 export default PageRoot
